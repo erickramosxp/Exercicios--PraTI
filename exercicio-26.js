b0c827ecf976216fa3d4138639ce7e050b0644f5
Erick Ramos <erickramossilva@gmail.com>
2024-06-09 11:59:40 -0300

    finalizando

--- exercicio-26.js
+++ exercicio-26.js
@@ -2,3 +2,45 @@
     26. Dadas duas matrizes numéricas A[1..3,1..5] e B[1..3,1..5], calcular a matriz produto
     P[1..3,1..5]. 
 */
+
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  const matriz = [];
+  const matriz2 = [];
+
+function preencherMatriz(matriz) {
+  for (let i = 0; i < 3; i++) {
+const linha = [];
+    for (let j = 0; j < 5; j++) {
+      linha.push(gerarNumeroAleatorio(1, 10));
+    }
+    matriz.push(linha);
+    }
+}
+
+preencherMatriz(matriz);
+preencherMatriz(matriz2);
+
+console.log("Duas Matrizes: ");
+
+console.log(matriz);
+console.log(matriz2);
+
+function calcularProdutoEntreMatriz(matriz, matriz2) {
+    let novaMatriz = [];
+    for (let i = 0; i < matriz.length && i < matriz2.length; i++) {
+        let linha = [];
+        for (let j = 0; j < matriz[i].length && matriz2[i].length; j++) {
+            linha.push(matriz[i][j] * matriz2[i][j]);
+        }
+        novaMatriz.push(linha);
+    }
+    return (novaMatriz);
+}
+
+let novaMatriz = calcularProdutoEntreMatriz(matriz, matriz2);
+
+console.log("Matriz produto");
+console.log(novaMatriz);
--- exercicio-35.js
+++ exercicio-35.js
@@ -7,4 +7,59 @@
 
 const prompt = require('prompt-sync')();
 
-let numerosString = prompt("Digite ");
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+let vetor = [];
+let vetorImpar = [];
+let vetorPar = [];
+
+for (let i = 0; i < 30; i++) {
+      vetor.push(gerarNumeroAleatorio(1, 99));
+}
+
+console.log(vetor);
+
+function adicionarNoVetorPar(numero) {
+    if (numero % 2 == 0 && vetorPar.length == 5) {
+        console.log("Vetor par já preenchido.");
+        return ;
+    }
+    if (vetorPar.length < 5 && numero % 2 == 0) {
+        vetorPar.push(numero);
+        if (vetorPar.length == 5) {
+            console.log("vetor par: " + vetorPar);
+        }
+    }
+}
+
+function adicionarNoVetorImpar(numero) {
+    if (numero % 2 != 0 && vetorImpar.length == 5) {
+        console.log("Vetor impar já preenchido.");
+        return ;
+    }
+    if (vetorImpar.length < 5 && numero % 2 != 0) {
+        vetorImpar.push(numero);
+        if (vetorImpar.length == 5) {
+            console.log("vetor impar: " + vetorImpar);
+        }
+    }
+}
+
+function dividirMatrizEmImparEPar() {
+    while (vetorImpar.length < 5 || vetorPar.length < 5) {
+        let numero = parseInt(prompt("Digite um número para ser feita a sepacação: "));
+        if (!isNaN(numero) && numero != undefined) {
+            adicionarNoVetorImpar(numero);
+            adicionarNoVetorPar(numero);
+        } else {
+            console.log("Entrada invalida!");
+        }
+    }
+    console.log("Vetores totalmente preenchidos");
+    console.log("vetor par: " + vetorPar);
+    console.log("vetor impar: " + vetorImpar);
+}
+
+dividirMatrizEmImparEPar();
